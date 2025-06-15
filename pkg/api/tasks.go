7d5e9b49a3780c1461b287008ac898e9c10d5141
Tutunaru Stanislav <tutunaru.stanislav@yandex.ru>
2025-06-15 11:46:27 +0500

    removed comments

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -41,21 +41,3 @@ func (h *TasksHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	writeJson(w, TasksResponse{Tasks: tasks}, http.StatusOK)
 }
-
-// func tasksHandler(w http.ResponseWriter, r *http.Request) {
-// 	search := r.URL.Query().Get("search")
-// 	var date string
-// 	if len(search) > 0 {
-// 		parsedDate, err := time.Parse(InputDateFormat, search)
-// 		if err == nil {
-// 			date = parsedDate.Format(DateFormat)
-// 			search = ""
-// 		}
-// 	}
-
-// 	tasks, err := db.GetTasks(maxTasks, search, date)
-// 	if err != nil {
-// 		writeError(w, err.Error(), http.StatusInternalServerError)
-// 	}
-// 	writeJson(w, TasksResponse{Tasks: tasks}, http.StatusOK)
-// }

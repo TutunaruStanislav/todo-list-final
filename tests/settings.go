46a498b75ad8d5848678c7a3e3cc2d3446b85b0a
Tutunaru Stanislav <tutunaru.stanislav@yandex.ru>
2025-06-17 19:38:21 +0500

    updated README file

--- tests/settings.go
+++ tests/settings.go
@@ -1,7 +1,7 @@
 package tests
 
 var Port = 7540
-var DBFile = "../scheduler.db"
+var DBFile = "../data/scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTAwNzgzNDcsInVzZXJuYW1lIjoiIn0.h8emOLKSIWSGyMYLDOj81o0eniH489BfkU1UKz7h_wc`
